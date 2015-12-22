18f39a7519ad0f56ec9c66c3ab97731fe3ac3418
vietpn <vietpnk53@gmail.com>
2015-12-22 10:36:14 +0700

    update

--- intermediate/client/recipes/Recipe.js
+++ intermediate/client/recipes/Recipe.js
@@ -1,8 +1,20 @@
 /**
  * Created by vietpn on 21/12/2015.
  */
+Template.Recipe.helpers({
+    updateRecipeId: function(){
+        return this._id;
+    }
+});
+
 Template.Recipe.events({
     'click .toggle-menu': function() {
         Meteor.call('toggleMenuItem', this._id, this.inMenu);
+    },
+    'click .fa-trash': function() {
+        Meteor.call('deleteRecipe', this._id);
+    },
+    'click .fa-pencil': function() {
+        Session.set('editMode', !Session.get('editMode'))
     }
 });
